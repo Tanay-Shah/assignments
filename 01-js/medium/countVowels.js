ff4433146d91de196b5aa4fc16316fae0afa43fc
Tanay-Shah <100477819+Tanay-Shah@users.noreply.github.com>
2023-12-07 02:07:50 +0530

    Update countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -2,11 +2,23 @@
   Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
   Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
 
-  Once you've implemented the logic, test your code by running
+  Once you've implemented the logic, test your code by running|  npx jest ./tests/countVowels.test.js
 */
 
-function countVowels(str) {
-    // Your code here
+function countVowels(st) {
+    
+  let str = st.toLowerCase();
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+      if(str[i]==='a' ||str[i]==='e' ||str[i]==='i' ||str[i]==='o' ||str[i]==='u'){
+        count++;
+      }
+    }
+  }
+
+  return count;
 }
 
 module.exports = countVowels;
